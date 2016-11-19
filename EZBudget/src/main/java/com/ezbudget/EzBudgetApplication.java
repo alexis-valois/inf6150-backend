fc2b0af7c094edf17c77087461af963745c0592b
sergedelil <dogny.gnagnely_serge@courrier.uqam.ca>
2016-11-19 16:25:27 -0500

    mon fameux fichier

--- EZBudget/src/main/java/com/ezbudget/EzBudgetApplication.java
+++ EZBudget/src/main/java/com/ezbudget/EzBudgetApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ComponentScan
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class EzBudgetApplication {
 
 	public static void main(String[] args) {
+		args = new String[]{"--spring.profiles.active=serge"};
 		SpringApplication.run(EzBudgetApplication.class, args);
 	}
 }
